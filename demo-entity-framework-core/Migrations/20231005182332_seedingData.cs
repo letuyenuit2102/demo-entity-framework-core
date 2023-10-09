e9b47154624e38f63e8d7eb959575639e7ae6c4d
tuyendepzai <letuyenkhtn212@gmail.com>
2023-10-10 03:23:28 +0700

    seeding data

--- demo-entity-framework-core/Migrations/20231005182332_seedingData.cs
+++ demo-entity-framework-core/Migrations/20231005182332_seedingData.cs
@@ -10,12 +10,40 @@ namespace demo_entity_framework_core.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-        }
+            migrationBuilder.InsertData(
+            table: "categories",
+	        columns: new[] {"Id", "Name" },
+	        values: new object[] { "EFD2AB19-BD45-48E0-DA90-08DB9FB57EB7" , "Category A" }
+            );
+			migrationBuilder.InsertData(
+			table: "categories",
+			columns: new[] { "Id", "Name" },
+			values: new object[] { "D48B2247-0F86-4CAA-5784-08DB9FE27B9B", "Category B" }
+			);
+			migrationBuilder.InsertData(
+			table: "categories",
+			columns: new[] { "Id", "Name" },
+			values: new object[] { "1739F48C-2E6D-4DD4-1529-08DBC0BF125E", "Category C" }
+			);
+
+
+			migrationBuilder.InsertData(
+			table: "products",
+			columns: new[] { "Id", "Name" , "Description" , "Price" , "CategoryId"},
+			values: new object[] { "1739F48C-2E7D-4DD4-1529-08DBC0CD125E", "Product A" , "Description Product A" , 50.5 , "EFD2AB19-BD45-48E0-DA90-08DB9FB57EB7" }
+			);
+
+			migrationBuilder.InsertData(
+			table: "products",
+			columns: new[] {"Id", "Name", "Description", "Price", "CategoryId" },
+			values: new object[] { "1839F48C-2E7D-4DD5-1529-08DBC0CD125E", "Product B", "Description Product B", 125.4, "1739F48C-2E6D-4DD4-1529-08DBC0BF125E" }
+			);
+		}
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
 
-        }
+		}
     }
 }
